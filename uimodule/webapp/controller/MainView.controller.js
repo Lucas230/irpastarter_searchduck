feb356e453be7bb3f6ab7833eb58d7d2379d86e7
Lucas Pires <lucaspires230@gmail.com>
2021-12-14 11:03:29 -0300

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["irpastarter/searchDuck/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("irpastarter.searchDuck.controller.MainView", {});
+});
